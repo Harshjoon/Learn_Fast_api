6f8a42587a4d7c32b78f987d70eb481645562846
Harsh joon <harshjoon222@gmail.com>
2023-10-31 09:40:11 +0530

    authentication algorithm

---
+++ src_2/auth/auth.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from typing import Annotated, Union
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from sqlalchemy.orm import Session
+
+SECRET_KEY  = '13acbc59ef1e20241797a6383ae55edf8b2bb27e21e8d193a3528b42a05ab6e3'
+ALGORITHM   = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Not important now
+def create_access_token():
+    return None
